cbfcac2e3e284c7a294d52e10a3fbb8575c14f9d
Khaio <kaio.fonseca@decisaosistemas.com.br>
2023-12-26 11:41:52 -0300

    Update frontend styles and components

--- frontend/src/app/shared/components/breadcrumb/breadcrumb.component.ts
+++ frontend/src/app/shared/components/breadcrumb/breadcrumb.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-breadcrumb',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./breadcrumb.component.scss']
 })
 export class BreadcrumbComponent {
+  @Input() clienteItem: string = '';
+  @Input() currentPage: string = '';
 
 }
