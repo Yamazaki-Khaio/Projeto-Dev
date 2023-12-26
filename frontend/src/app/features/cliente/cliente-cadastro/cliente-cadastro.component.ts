efdb41bcdd99da1bb8585ef7693e7553a320cfc6
Khaio <kaio.fonseca@decisaosistemas.com.br>
2023-12-26 18:03:07 -0300

    Add dependencies and update form in user registration and client registration components

--- frontend/src/app/features/cliente/cliente-cadastro/cliente-cadastro.component.ts
+++ frontend/src/app/features/cliente/cliente-cadastro/cliente-cadastro.component.ts
@@ -1,5 +1,3 @@
-// cliente-cadastro.component.ts
-
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ClienteService } from '../cliente.service';
@@ -12,8 +10,9 @@ import { Cliente } from '../cliente';
   styleUrls: ['./cliente-cadastro.component.scss'],
 })
 export class ClienteCadastroComponent implements OnInit {
-  clienteForm: FormGroup = new FormGroup({});
 
+  clienteForm: FormGroup = new FormGroup({});
+  emailError: string = 'Digite um CNPJ/CPF válido';
 
   constructor(
     private fb: FormBuilder,
@@ -23,40 +22,46 @@ export class ClienteCadastroComponent implements OnInit {
 
   ngOnInit(): void {
     this.clienteForm = this.fb.group({
+      inputIdentificacao: ['', [Validators.required, Validators.pattern(/^\d{14}$|^\d{18}$/)]],
       inputNome: ['', [Validators.required]],
-      inputIdentificacao: ['', [Validators.required, Validators.minLength(11)]],
       inputRef: ['', [Validators.required]],
-      inputInscricaoMunicipal: [''],
-      inputInscricaoEstadual: [''],
     });
 
   }
 
   OnSubmit(): void {
-    const cliente: Cliente = {
-      nome: this.clienteForm.value.inputNome,
-      identificacao: this.clienteForm.value.inputIdentificacao,
-      nome_ref: this.clienteForm.value.inputRef,
-      inscricao_municipal: this.clienteForm.value.inputInscricaoMunicipal,
-      inscricao_estadual: this.clienteForm.value.inputInscricaoEstadual,
-    };
+    if (this.clienteForm.valid) {
+      const cliente: Cliente = {
+        identificacao: this.clienteForm.value.inputIdentificacao,
+        nome: this.clienteForm.value.inputNome,
+        nome_ref: this.clienteForm.value.inputRef,
+      };
 
-    this.clienteService.createCliente(cliente).subscribe(
-      (data: Cliente) => {
-        console.log(data);
-        this.router.navigate(['/profile/cliente/editar/' + data.id]);
-      },
-      (error: any) => {
-        console.log(cliente);
-        console.log(error);
-      }
-    );
+      this.clienteService.createCliente(cliente).subscribe(
+        (data: Cliente) => {
+          console.log(data);
+          this.router.navigate(['/profile/cliente/editar/' + data.id]);
+        },
+        (error: any) => {
+          console.log(cliente);
+          console.log(error);
+        }
+      );
+    } else {
+      console.log('Erro no formulário. Verifique os campos.');
+    }
+  }
+
+  isCpfCnpjValid(): boolean {
+    const cpfCnpj = this.clienteForm.value.inputIdentificacao.replace(/\D/g, '');
+
+    return cpfCnpj.length === 14 || cpfCnpj.length === 18;
+  }
+
+  voltar() {
+    this.router.navigate(['users/profile/cliente/']);
   }
 
 
-
-  voltar() {
-      this.router.navigate(['users/profile/cliente/']);
-    }
 
 }
