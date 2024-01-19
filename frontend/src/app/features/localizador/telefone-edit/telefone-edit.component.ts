11c82acd2c6008c130a0658bebf96248c2d186fa
Khaio <kaio.fonseca@decisaosistemas.com.br>
2024-01-19 09:31:40 -0300

    Update telefone-edit component with new form and functionality

--- frontend/src/app/features/localizador/telefone-edit/telefone-edit.component.ts
+++ frontend/src/app/features/localizador/telefone-edit/telefone-edit.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TelefoneService } from '../telefone.service'; // Certifique-se de importar o serviço correto
+import { ActivatedRoute } from '@angular/router';
+import { Telefone } from '../telefone';
+import { AlertService } from './../../../shared/services/alert.service';
 
 @Component({
   selector: 'app-telefone-edit',
   templateUrl: './telefone-edit.component.html',
-  styleUrls: ['./telefone-edit.component.scss']
+  styleUrls: ['../telefone-cadastro/telefone-cadastro.component.scss'] // Certifique-se de ajustar o caminho do estilo conforme necessário
 })
 export class TelefoneEditComponent {
+  @Output() telefoneAdicionado = new EventEmitter<any>();
+  userId!: number; // Declare a propriedade userId como um Input
+  telefoneId!: number;
 
+  telefoneForm!: FormGroup;
+  isTemplateHidden: boolean = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private telefoneService: TelefoneService,
+    private route: ActivatedRoute,
+    private alertService: AlertService,
+  ) { }
+
+  ngOnInit() {
+    this.telefoneService.getTelefone(this.telefoneId).subscribe(
+      (telefone: Telefone) => {
+        this.telefoneForm = this.formBuilder.group({
+          inputTelefone: [telefone.tel, [Validators.required, Validators.pattern('[0-9]+')]],
+          isPrincipal: [telefone.is_principal, Validators.required]
+        });
+      },
+      (error: any) => {
+        console.error('Erro ao carregar telefone:', error);
+      }
+    );
+  }
+
+  cancelar() {
+    this.isTemplateHidden = true;
+  }
+
+  adicionarTelefone() {
+    console.log('Adicionando telefone:', this.telefoneForm.value);
+    if (this.telefoneForm.valid) {
+      const telefoneData: Telefone = {
+        tel: this.telefoneForm.value.inputNumero,
+        is_principal: this.telefoneForm.value.isPrincipal,
+      };
+
+      this.telefoneService.updateTelefone(this.userId, telefoneData).subscribe(
+        response => {
+          console.log('Telefone adicionado com sucesso:', response);
+          this.alertService.showAlert('Telefone adicionado com sucesso.', 'alert-primary');
+          this.telefoneAdicionado.emit(response);
+          this.telefoneForm.reset();
+          this.isTemplateHidden = true;
+        },
+        error => {
+          this.alertService.showAlert('Erro ao adicionar telefone.', 'alert-danger');
+          this.telefoneAdicionado.emit(error);
+          console.error('Erro ao adicionar telefone:', error);
+        }
+      );
+    }
+  }
 }
